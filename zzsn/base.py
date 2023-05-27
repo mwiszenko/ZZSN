6fda81180bc2ed73be4a4fa04533213410e6e802
Michał Wiszenko <wiszenkom@gmail.com>
2023-05-27 21:43:35 +0200

    add type hints

--- zzsn/base.py
+++ zzsn/base.py
@@ -1,4 +1,4 @@
-import glob
+from glob import glob
 
 import pandas as pd
 from PIL import Image
@@ -11,30 +11,36 @@ NAME = "zzsn"
 
 class CustomImageDataset(Dataset):
     def __init__(
-        self, annotations_file, data_dir, transform=None, target_transform=None
+        self,
+        annotations_file: str,
+        data_dir: str,
+        transform: callable = None,
+        target_transform: callable = None,
     ):
-        classes = pd.read_csv(annotations_file, names=["class"])
-        y_classes = []
-        y_files = []
+        classes: pd.DataFrame = pd.read_csv(annotations_file, names=["class"])
+        y_classes: list = []
+        y_files: list = []
 
         for i, lab in classes.iterrows():
             alphabet, character, _ = lab["class"].split("/")
-            img_dir = os.path.join(data_dir, alphabet, character)
-            files = sorted(glob.glob(os.path.join(img_dir, "*.png")))
+            img_dir: str = os.path.join(data_dir, alphabet, character)
+            files: list[str] = sorted(glob(os.path.join(img_dir, "*.png")))
             y_files.extend(files)
             y_classes.extend([lab["class"]] * len(files))
 
-        self.img_labels = pd.DataFrame({"file": y_files, "class": y_classes})
-        self.data_dir = data_dir
-        self.transform = transform
-        self.target_transform = target_transform
+        self.img_labels: pd.DataFrame = pd.DataFrame(
+            {"file": y_files, "class": y_classes}
+        )
+        self.data_dir: str = data_dir
+        self.transform: callable = transform
+        self.target_transform: callable = target_transform
 
     def __len__(self):
         return len(self.img_labels)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int):
         image = read_image(self.img_labels.iloc[idx, 0])
-        label = self.img_labels.iloc[idx, 1]
+        label: str = self.img_labels.iloc[idx, 1]
         if self.transform:
             _, _, rotation = label.split("/")
             image = self.transform(image, rotation)
@@ -47,15 +53,17 @@ def read_image(path: str):
     return Image.open(path)
 
 
-def transform_image(img, rot):
-    rotated_img = img.rotate(float(rot[3:]))
-    scaled_img = rotated_img.resize((IMG_HEIGHT, IMG_WIDTH))
-    return scaled_img
+def transform_image(img, rot: str):
+    # rotation
+    transformed_img = img.rotate(float(rot[3:]))
+    # scaling
+    transformed_img = transformed_img.resize((IMG_HEIGHT, IMG_WIDTH))
+    return transformed_img
 
 
 def create_dataset(split: str):
     print("Loading {} dataset...".format(split))
-    ds = CustomImageDataset(
+    ds: CustomImageDataset = CustomImageDataset(
         annotations_file=os.path.join(OMNIGLOT_SPLITS_DIR, split + ".txt"),
         data_dir=OMNIGLOT_DATA_DIR,
         transform=transform_image,
--- zzsn/cli.py
+++ zzsn/cli.py
@@ -2,8 +2,6 @@ from zzsn.base import CustomImageDataset, create_dataset
 
 
 def main():  # pragma: no cover
-    ds: CustomImageDataset = create_dataset("val")
-    print(len(ds))
-    print(ds.img_labels.head(21))
-    print(ds[20][0].show())
-    print("   Done")
+    ds_train: CustomImageDataset = create_dataset("train")
+    ds_val: CustomImageDataset = create_dataset("val")
+    ds_test: CustomImageDataset = create_dataset("test")
