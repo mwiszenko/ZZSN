101ebfbfe6c6191cdc048d58eca4ff28a38305e7
Michał Wiszenko <wiszenkom@gmail.com>
2023-06-02 15:05:32 +0200

    rename training mode variable

--- zzsn/cli.py
+++ zzsn/cli.py
@@ -25,22 +25,26 @@ def main():
     parser = argparse.ArgumentParser()
     modes = parser.add_subparsers(dest="command", required=True)
 
-    train = modes.add_parser("train")
-    train.add_argument("--epochs", "-e", type=int, default=5)
-    train.add_argument("--n_way", "-nw", type=int, default=5)
-    train.add_argument("--n_support", "-ns", type=int, default=5)
-    train.add_argument("--n_query", "-nq", type=int, default=5)
-    train.add_argument("--n_train_episodes", "-nte", type=int, default=100)
-    train.add_argument("--n_eval_episodes", "-nee", type=int, default=10)
-    train.add_argument("--learning_rate", "-lr", type=float, default=0.001)
-    train.add_argument(
+    train_mode = modes.add_parser("train")
+    train_mode.add_argument("--epochs", "-e", type=int, default=5)
+    train_mode.add_argument("--n_way", "-nw", type=int, default=5)
+    train_mode.add_argument("--n_support", "-ns", type=int, default=5)
+    train_mode.add_argument("--n_query", "-nq", type=int, default=5)
+    train_mode.add_argument(
+        "--n_train_episodes", "-nte", type=int, default=100
+    )
+    train_mode.add_argument("--n_eval_episodes", "-nee", type=int, default=10)
+    train_mode.add_argument(
+        "--learning_rate", "-lr", type=float, default=0.001
+    )
+    train_mode.add_argument(
         "--distance_func",
         "-dist",
         type=str,
         choices=["euclidean"],
         default="euclidean",
     )
-    train.set_defaults(func=ModeMapper.train)
+    train_mode.set_defaults(func=ModeMapper.train)
 
     args = parser.parse_args()
     args.func(args)
