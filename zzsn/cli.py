78e980a2aed2607dde161ebfb54dfd84cc5ef094
Michał Wiszenko <wiszenkom@gmail.com>
2023-06-02 15:11:50 +0200

    move default cli values into constants

--- zzsn/cli.py
+++ zzsn/cli.py
@@ -1,5 +1,16 @@
 import argparse
 
+from zzsn.constants import (
+    DEFAULT_DISTANCE_FUNC,
+    DEFAULT_EPOCHS,
+    DEFAULT_LEARNING_RATE,
+    DEFAULT_N_EVAL_EPISODES,
+    DEFAULT_N_QUERY,
+    DEFAULT_N_SUPPORT,
+    DEFAULT_N_TRAIN_EPISODES,
+    DEFAULT_N_WAY,
+    DISTANCE_FUNCTIONS,
+)
 from zzsn.train import run_train
 
 
@@ -26,23 +37,32 @@ def main():
     modes = parser.add_subparsers(dest="command", required=True)
 
     train_mode = modes.add_parser("train")
-    train_mode.add_argument("--epochs", "-e", type=int, default=5)
-    train_mode.add_argument("--n_way", "-nw", type=int, default=5)
-    train_mode.add_argument("--n_support", "-ns", type=int, default=5)
-    train_mode.add_argument("--n_query", "-nq", type=int, default=5)
+    train_mode.add_argument("--epochs", "-e", type=int, default=DEFAULT_EPOCHS)
+    train_mode.add_argument("--n_way", "-nw", type=int, default=DEFAULT_N_WAY)
     train_mode.add_argument(
-        "--n_train_episodes", "-nte", type=int, default=100
+        "--n_support", "-ns", type=int, default=DEFAULT_N_SUPPORT
     )
-    train_mode.add_argument("--n_eval_episodes", "-nee", type=int, default=10)
     train_mode.add_argument(
-        "--learning_rate", "-lr", type=float, default=0.001
+        "--n_query", "-nq", type=int, default=DEFAULT_N_QUERY
+    )
+    train_mode.add_argument(
+        "--n_train_episodes",
+        "-nte",
+        type=int,
+        default=DEFAULT_N_TRAIN_EPISODES,
+    )
+    train_mode.add_argument(
+        "--n_eval_episodes", "-nee", type=int, default=DEFAULT_N_EVAL_EPISODES
+    )
+    train_mode.add_argument(
+        "--learning_rate", "-lr", type=float, default=DEFAULT_LEARNING_RATE
     )
     train_mode.add_argument(
         "--distance_func",
         "-dist",
         type=str,
-        choices=["euclidean"],
-        default="euclidean",
+        choices=DISTANCE_FUNCTIONS,
+        default=DEFAULT_DISTANCE_FUNC,
     )
     train_mode.set_defaults(func=ModeMapper.train)
 
